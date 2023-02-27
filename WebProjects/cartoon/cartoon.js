f385dd7991ddb11222be35cac3d3a5edf1d1577a
AnyiC <anyichen212@gmail.com>
2023-02-27 16:44:44 -0500

    n/a

--- WebProjects/cartoon/cartoon.js
+++ WebProjects/cartoon/cartoon.js
@@ -3,7 +3,9 @@ var context;
 
 function canvasColor(){
     canvas = document.getElementById("myCanvas");
+    context = canvas.getContext("2d")
     canvas.fillStyle = "blue";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 document.addEventListener('DOMContentLoaded', canvasColor);
