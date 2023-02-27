620c7d2734ad1f70ae4d5931a4d11fda945a8c5c
AnyiC <55215189+anyichen212@users.noreply.github.com>
2023-02-27 18:41:55 -0500

    Update cartoon.js

--- cartoon/cartoon.js
+++ cartoon/cartoon.js
@@ -3,7 +3,7 @@ var context;
 
 function canvasColor(){
     canvas = document.getElementById("myCanvas");
-    context = canvas.getContext("2d")
+    context = canvas.getContext("2d");
     canvas.fillStyle = "blue";
     context.fillRect(0, 0, canvas.width, canvas.height);
 }
