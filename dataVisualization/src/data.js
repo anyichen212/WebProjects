f26647f33978948ea9d431e384b8868e78018426
AnyiC <anyichen212@gmail.com>
2023-04-14 14:44:55 -0400

    init commit

---
+++ dataVisualization/src/data.js
@@ -0,0 +1,43 @@
+
+//donut chart
+new Chart(document.getElementById("doughnut-chart").getContext("2d"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Anyi Chen'
+      }
+    }
+});
+
+//horizontal bar chart
+new Chart(document.getElementById("bar-chart-horizontal"), {
+  type: 'horizontalBar',
+  data: {
+    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+    datasets: [
+      {
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }
+    ]
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: false,
+      text: 'Predicted world population (millions) in 2050'
+    }
+  }
+});
