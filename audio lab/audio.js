044d423d7ce522b61c6eb5ce962a026c6440a5de
AnyiC <anyichen212@gmail.com>
2023-05-21 03:56:38 -0400

    audio lab fin

---
+++ audio lab/audio.js
@@ -0,0 +1,25 @@
+var x = document.getElementById("myAudio");
+var y = document.getElementById("audio2");
+var z = document.getElementById("audio3");
+var xmas = document.getElementById("audio4");
+var a = document.getElementById("audio5");
+
+function playAudio() {
+    x.play();
+}
+
+function playAudio2() {
+    y.play();
+}
+
+function playAudio3() {
+    z.play();
+}
+
+function playAudio4() {
+    xmas.play();
+}
+
+function playAudio5() {
+    a.play();
+}
